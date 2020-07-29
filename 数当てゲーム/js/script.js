c2b27089740c79c64a20b23a2715aa6dfd99e34a
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2020-07-29 18:29:12 +0900

    ueda fix

--- 数当てゲーム/js/script.js
+++ 数当てゲーム/js/script.js
@@ -3,17 +3,17 @@ var kotae
 var sukoa
 function gameinit(){
         kotae=Math.floor(Math.random()*3+1)
-        sukoa=200
+        sukoa=200   ; alert( "kotae :" +kotae);
 }
 
 function numbar(p){
     var pname="p"+p
     var item=document.getElementById(pname)
     if(p==kotae){
-        item.src="img/当たり.jpg"
+        item.src="img/当たり.jpg"   //多分、日本語のファイル名はNGのはず
         alert("atari")
     }else{
-        item.src="img/はずれ.jpg"
+        item.src="img/はずれ.jpg"   //多分、日本語のファイル名はNGのはず
         alert("hazure")
         sukoa=sukoa-20
     }
